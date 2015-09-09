e6d2a57a4b13d3b8765d056bd9d3b7533a17513f
MircoT <mirco.theone@gmail.com>
2015-09-09 18:54:06 +0200

    Fixed CSS and js script

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,29 @@
-console.log('This would be the main JS file.');
+'use strict'
+
+;(function () {
+  window.onload = function () {
+    // get image list
+    var imageList = document.getElementById('imageList')
+    // get elements
+    var images = imageList.getElementsByTagName('img')
+    // if have some images
+    if (images && images.length) {
+      // first id
+      var imageCurrent = 0
+      // hide all
+      for ( var key = 0; key != images.length; ++key) {
+        images[key].style.display = 'none'
+      }
+      // show first
+      images[imageCurrent].style.display = 'block'
+      // change image event
+      imageList.onclick = function () {
+        if (images) {
+          images[imageCurrent].style.display = 'none'
+          imageCurrent = (imageCurrent + 1) % images.length
+          images[imageCurrent].style.display = 'block'
+        }
+      }
+    }
+  }
+})()
