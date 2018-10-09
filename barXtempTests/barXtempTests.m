a4c95576408b08ace53327a528c910d6dfa2e02d
MircoT <mirco.theone@gmail.com>
2018-10-09 11:32:04 +0200

    Add test schema (xcode10)

---
+++ barXtempTests/barXtempTests.m
@@ -0,0 +1,37 @@
+//
+//  barXtempTests.m
+//  barXtempTests
+//
+//  Created by Mirco Tracolli on 09/10/2018.
+//  Copyright © 2018 Gabriele Di Bari. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface barXtempTests : XCTestCase
+
+@end
+
+@implementation barXtempTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
